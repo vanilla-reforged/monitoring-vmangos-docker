423e072a481a1d6c8a9986c596b64981f5ac41c2
vanilla-reforged <vanilla.reforged@outlook.com>
2024-09-17 16:22:26 +0200

    Update 21-daily-alert-test.sh

--- 21-daily-alert-test.sh
+++ 21-daily-alert-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "heartbeat{job=\"manual\"} 1" | curl --data-binary @- http://localhost:9091/metrics/job/heartbeat
+echo "heartbeat{job=\"manual\"} 1" | curl --data-binary @- http://127.0.0.1:9091/metrics/job/heartbeat
